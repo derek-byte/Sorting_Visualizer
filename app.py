96ca1b7f8ad1c51314ebd14e8e1b953bca063f71
derek-byte <derek.s.prog@gmail.com>
2022-01-20 22:13:39 -0500

    Create CSV function

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+from os import sendfile
 from select import select
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from flask_sqlalchemy import SQLAlchemy 
 from sorting_alg.merge_sort import MergeSort
 from sorting_alg.quick_sort import QuickSort
 from sorting_alg.selection_sort import SelectionSort
 from sorting_alg.bubble_sort import BubbleSort
 from sorting_alg.insertion_sort import InsertionSort
+import csv
 
 app = Flask(__name__)
+app.run(debug=True, use_debugger=False, use_reloader=False)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,7 +22,6 @@ class Sort(db.Model):
     title = db.Column(db.String(100))
     past_array = db.Column(db.String(10000))
     steps_array = db.Column(db.String(10000))
-    # complete = db.Column(db.Boolean)
 
 
 def convert_array(value):
@@ -44,39 +46,45 @@ def convert_array(value):
 def sort1(select, array):
     if select == "merge_sort":
         sorting_alg = "Merge Sort"
+        id = "merge_sort"
         alg = MergeSort()
         alg.mergeSort(array.copy(), 0, len(array)-1)
         array_str = str(alg.sorting_steps)
 
     elif select == "bubble_sort":
         sorting_alg = "Bubble Sort"
+        id = "bubble_sort"
         alg = BubbleSort()
         alg.bubbleSort(array.copy())
         array_str = str(alg.sorting_steps)
 
     elif select == "selection_sort":
         sorting_alg = "Selection Sort"
+        id = "selection_sort"
         alg = SelectionSort()
         alg.selectionSort(array.copy())
         array_str = str(alg.sorting_steps)
 
     elif select == "insertion_sort":
         sorting_alg = "Insertion Sort"
+        id = "insertion_sort"
         alg = InsertionSort()
         alg.insertionSort(array.copy())
         array_str = str(alg.sorting_steps)
 
     elif select == "quick_sort":
         sorting_alg = "Quick Sort"
+        id = "quick_sort"
         alg = QuickSort()
         alg.quickSort(array.copy(), 0, len(array)-1)
         array_str = str(alg.sorting_steps)
 
     else:
+        id = "None"
         sorting_alg = "None"
         array_str = "None"
 
-    return sorting_alg, array_str
+    return sorting_alg, array_str, id
 
 @app.route("/", methods = ["POST", "GET"])
 def home():
@@ -94,8 +102,9 @@ def home():
 
         sorting_alg = result[0]
         array_str = result[1]
+        id = result[2]
 
-        new_sort = Sort(title=sorting_alg, past_array=str(array), steps_array=array_str)
+        new_sort = Sort(title=id, past_array=str(array), steps_array=array_str)
         db.session.add(new_sort)
         db.session.commit()
         print(sort_list)
@@ -109,13 +118,10 @@ def home():
 @app.route("/update/<int:new_sort_id>", methods = ["POST", "GET"])
 def update(new_sort_id):
         sort = Sort.query.filter_by(id=new_sort_id).first()
-        # array = sort.
-        print(request.method)
-        print(new_sort_id)
         if request.method == "POST":
             value = request.form["number"]
             array = convert_array(value)
-            result = sort1(select, array)
+            result = sort1(sort.title, array)
             array_str = result[1]
             sort.past_array = str(array)
             sort.steps_array = array_str
@@ -129,3 +135,17 @@ def delete(new_sort_id):
         db.session.delete(sort)
         db.session.commit()
         return redirect(url_for("home"))
+
+@app.route("/create_csv")
+def create_csv():
+    sort_list = Sort.query.all()
+    csv_arr = []
+    for new_sort in sort_list[::-1]:
+        csv_arr.append([new_sort.id, new_sort.title, new_sort.past_array, new_sort.steps_array])
+
+    with open('sorting_data.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        for item in csv_arr:
+            writer.writerow(item)
+        return send_file('sorting_data.csv', as_attachment=True)
+            
