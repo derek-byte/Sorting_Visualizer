88e76b6997e50d8167df1050c0563a4ee5f8a093
derek-byte <derek.s.prog@gmail.com>
2022-01-21 17:17:28 -0500

    formating code to pep8 standards and renaming variables

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from os import sendfile
-from select import select
 from flask import Flask, render_template, request, redirect, url_for, send_file
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from sorting_alg.merge_sort import MergeSort
 from sorting_alg.quick_sort import QuickSort
 from sorting_alg.selection_sort import SelectionSort
@@ -17,68 +15,70 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Sort(db.Model):
+
+class SortingEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     past_array = db.Column(db.String(10000))
     steps_array = db.Column(db.String(10000))
 
+# Creates all tables in SQLAlchemy
+db.create_all()
 
-def convert_array(value):
+def convert_str_to_array(value):
     if value == "":
         return []
 
     array = []
     i = 0
-    num = ""
+    curr_str_num = ""
     last_comma = 0
+
     while i < len(value):
-      if value[i] == ",":
-        array.append(int(num))
-        last_comma = i
-        num = ""
-      else:
-          num += value[i]
-      i += 1 
-    array.append(int(value[last_comma+1:]))
+        if value[i] == ",":
+            array.append(int(curr_str_num))
+            last_comma = i
+            curr_str_num = ""
+        else:
+            curr_str_num += value[i]
+        i += 1
+
+    # Append last number to array
+    array.append(int(value[last_comma + 1:]))
     return array
 
-def sort1(select, array):
+
+def select_sorting_alg(select, array):
     if select == "merge_sort":
         sorting_alg = "Merge Sort"
         id = "merge_sort"
         alg = MergeSort()
-        alg.mergeSort(array.copy(), 0, len(array)-1)
+        alg.mergeSort(array.copy(), 0, len(array) - 1)
         array_str = str(alg.sorting_steps)
-
     elif select == "bubble_sort":
         sorting_alg = "Bubble Sort"
         id = "bubble_sort"
         alg = BubbleSort()
         alg.bubbleSort(array.copy())
         array_str = str(alg.sorting_steps)
-
     elif select == "selection_sort":
         sorting_alg = "Selection Sort"
         id = "selection_sort"
         alg = SelectionSort()
         alg.selectionSort(array.copy())
         array_str = str(alg.sorting_steps)
-
     elif select == "insertion_sort":
         sorting_alg = "Insertion Sort"
         id = "insertion_sort"
         alg = InsertionSort()
         alg.insertionSort(array.copy())
         array_str = str(alg.sorting_steps)
-
     elif select == "quick_sort":
         sorting_alg = "Quick Sort"
         id = "quick_sort"
         alg = QuickSort()
-        alg.quickSort(array.copy(), 0, len(array)-1)
+        alg.quickSort(array.copy(), 0, len(array) - 1)
         array_str = str(alg.sorting_steps)
-
     else:
         id = "None"
         sorting_alg = "None"
@@ -86,66 +86,72 @@ def sort1(select, array):
 
     return sorting_alg, array_str, id
 
-@app.route("/", methods = ["POST", "GET"])
-def home():
-    db.create_all()
 
-    sort_list = Sort.query.all()
-    print(sort_list)
+@app.route("/", methods=["POST", "GET"])
+def home():
+    # Getting all sorting entries from database
+    all_sorting_entries = SortingEntry.query.all()
+
     if request.method == "POST":
         value = request.form["numbers"]
         select = request.form["sorting"]
-        array = convert_array(value)
+        array = convert_str_to_array(value)
         sorting_alg = ""
 
-        result = sort1(select, array)
+        result = select_sorting_alg(select, array)
 
         sorting_alg = result[0]
         array_str = result[1]
         id = result[2]
 
-        new_sort = Sort(title=id, past_array=str(array), steps_array=array_str)
+        new_sort = SortingEntry(
+            title=id,
+            past_array=str(array),
+            steps_array=array_str)
         db.session.add(new_sort)
         db.session.commit()
-        print(sort_list)
+        print(all_sorting_entries)
 
-        return render_template('index.html', sorting= sorting_alg, original=value, arr=array_str, sort_lists=sort_list)
+        return render_template('index.html', sorting=sorting_alg,
+                               original=value, arr=array_str, sort_lists=all_sorting_entries)
 
     else:
-        return render_template('index.html', sort_lists=sort_list)
+        return render_template('index.html', sort_lists=all_sorting_entries)
 
 
-@app.route("/update/<int:new_sort_id>", methods = ["POST", "GET"])
+@app.route("/update/<int:new_sort_id>", methods=["POST", "GET"])
 def update(new_sort_id):
-        sort = Sort.query.filter_by(id=new_sort_id).first()
-        if request.method == "POST":
-            value = request.form["number"]
-            array = convert_array(value)
-            result = sort1(sort.title, array)
-            array_str = result[1]
-            sort.past_array = str(array)
-            sort.steps_array = array_str
-        db.session.commit()
-        return redirect(url_for("home"))
+    sort = SortingEntry.query.filter_by(id=new_sort_id).first()
+    if request.method == "POST":
+        value = request.form["number"]
+        array = convert_str_to_array(value)
+        result = select_sorting_alg(sort.title, array)
+        array_str = result[1]
+        sort.past_array = str(array)
+        sort.steps_array = array_str
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 @app.route("/delete/<int:new_sort_id>")
 def delete(new_sort_id):
-        sort = Sort.query.filter_by(id=new_sort_id).first()
-        db.session.delete(sort)
-        db.session.commit()
-        return redirect(url_for("home"))
+    sort = SortingEntry.query.filter_by(id=new_sort_id).first()
+    db.session.delete(sort)
+    db.session.commit()
+    return redirect(url_for("home"))
+
 
 @app.route("/create_csv")
 def create_csv():
-    sort_list = Sort.query.all()
+    sort_list = SortingEntry.query.all()
     csv_arr = []
     for new_sort in sort_list[::-1]:
-        csv_arr.append([new_sort.id, new_sort.title, new_sort.past_array, new_sort.steps_array])
+        csv_arr.append([new_sort.id, new_sort.title,
+                       new_sort.past_array, new_sort.steps_array])
 
     with open('sorting_data.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
         for item in csv_arr:
             writer.writerow(item)
-        return send_file('sorting_data.csv', as_attachment=True)
-            
+
+    return send_file('sorting_data.csv', as_attachment=True)
